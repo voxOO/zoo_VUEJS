845fe88c399c4a8feeef190cb7622b5e67ee7a1a
Vojislav <vojislav.stupar@gmail.com>
2019-06-25 19:14:02 +0200

    tabela zivotinja

--- src/components/zooapp.vue
+++ src/components/zooapp.vue
@@ -1,12 +1,25 @@
 <template>
-    
+    <div>
+        <h3>Lista Zivotinja</h3>
+        <table>
+            <tr v-for="(zivotinja, index) in spisak_zivotinja" :key="index" >
+                <td>{{ zivotinja.vrsta }}</td><td>{{ zivotinja.ime}}</td><td>{{ zivotinja.datum_rodjenja }}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
     data () {
         return {
-
+            spisak_zivotinja: [
+                {vrsta:'Zec' , ime:'Svetislav' , datum_rodjenja: '1.1.1990'},
+                {vrsta:'Konj' , ime:'Cvetko' , datum_rodjenja: '4.10.2000'},
+                {vrsta:'Koza' , ime:'Dragica' , datum_rodjenja: '5.5.2006'},
+                {vrsta:'Glista' , ime:'Miroslav' , datum_rodjenja: '4.6.2018'},
+                {vrsta:'Pcela' , ime:'Rajka' , datum_rodjenja: '8.1.1980'},
+            ]
         }
     },
 
@@ -15,3 +28,21 @@ export default {
     }
 }
 </script>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
